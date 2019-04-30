b4d2c3278bbeae489eae94771bdce2d5c504d341
Mohit Yadav <mohit.yadav@xebia.com>
2019-05-01 01:42:08 +0400

    setup basic configs for different envs

--- src/server/app.js
+++ src/server/app.js
@@ -1,1 +1,9 @@
-    
+import express from 'express';
+import config from 'config';
+
+const app = express();
+const port = config.get('PORT');
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
