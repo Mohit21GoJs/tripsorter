87c6c45e0907b51dcbff9c4663c475edce8c60b5
Mohit Yadav <mohit.yadav@xebia.com>
2019-05-01 16:46:09 +0400

    update relative resolvve module paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,15 @@
-module.exports = { extends: 'airbnb-base' };
+module.exports = {
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    'import/resolver': { 'babel-module': {} },
+  },
+  plugins: [
+    'import',
+  ],
+};
---
+++ packages/README.md
@@ -0,0 +1,1 @@
+#### Custom Packages which can be imported in app and in future can be published to npm/yarn
